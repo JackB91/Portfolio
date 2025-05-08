1c04271bca3d1bb28802b42146279aed2fd65cb4
JackB91 <you@example.com>
2025-05-07 21:24:46 -0700

    disable scroll button display when already at top

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function scrollUp() {
 }
 
 scroll_top.addEventListener("click", scrollUp);
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    scroll_top.style.display = "block";
+  } else {
+    scroll_top.style.display = "none";
+  }
+});
